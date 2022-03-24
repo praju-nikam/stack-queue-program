bd908385685de64ba05428bace7091220a293f95
praju <sawantprajakta84@gmail.com>
2022-03-24 20:11:42 +0530

    Added Stack Queue Program

---
+++ src/com/java/bridgelabs/QueueClass.java
@@ -0,0 +1,70 @@
+package com.java.bridgelabs;
+
+public class QueueClass {
+
+    static class Node {
+        String data;
+        Node next;
+
+        //Create Constructor
+        Node(String data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    static class Queue {
+        static Node head = null;
+        static Node tail = null;
+
+        public static boolean isEmpty() {
+            return head == null & tail == null;
+        }
+
+        //Enqueue - Add Element in Queue
+        public static void add(String data) {
+            Node newNode = new Node(data);
+
+            if (tail == null) {
+                tail = head = newNode;
+                return;
+            }
+            tail.next = newNode;
+            tail = newNode;
+        }
+
+        //Dequeue  - Delete Element in Queue
+        public static String remove() {
+            if (isEmpty()) {
+                System.out.println("Empty Queue");
+            }
+            String front = head.data;
+            if (head == tail) {
+                tail = null;
+            }
+            head = head.next;
+            return front;
+        }
+
+        // peek - Peek The Element
+        public static String peek() {
+            if (isEmpty()) {
+                System.out.println("Empty Queue");
+            }
+            return head.data;
+        }
+    }
+
+    public static void main(String[] args) {
+        Queue queue = new Queue();
+        Queue.add("Praju");
+        Queue.add("Vaibhav");
+        Queue.add("Anvi");
+        Queue.add("Rohit");
+
+        while (! Queue.isEmpty()) {
+            System.out.println(Queue.peek());
+            Queue.remove();
+        }
+    }
+}
---
+++ src/com/java/bridgelabs/StackClass.java
@@ -0,0 +1,64 @@
+package com.java.bridgelabs;
+
+public class StackClass {
+    //create class
+    static class Node {
+       // variables
+        String data;
+        Node next;
+
+        // create constructor for Node Class
+        public Node(String data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    static class Stack {
+        public static Node head;          //variable
+
+        // create function isEmpty()
+        public static boolean isEmpty() {
+            return head == null;
+        }
+
+        public static void push(String data) {
+            Node newNode = new Node(data);
+            if (isEmpty()) {
+                head = newNode;
+                return;
+            }
+            newNode.next = head;
+            head = newNode;
+        }
+
+        public static String peek() {
+            if (isEmpty()) {
+                System.out.println("List is Empty");
+            }
+            return head.data;
+        }
+
+        public String pop() {
+            if (isEmpty()) {
+                System.out.println("List is Empty");
+            }
+            String top = head.data;
+            head = head.next;
+            return top;
+        }
+    }
+
+    public static void main(String[] args) {
+        Stack stack = new Stack();
+        Stack.push("praju");
+        Stack.push("Anvi");
+        Stack.push("Rohit");
+        Stack.push("Vaibhav");
+
+        while (!Stack.isEmpty()) {
+            System.out.println(Stack.peek());
+            stack.pop();
+        }
+    }
+}
